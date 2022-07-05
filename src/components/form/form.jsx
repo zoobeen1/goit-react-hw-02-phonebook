fcf6785e54b9700d2ae79784b24405088a532340
Vlad Zubin <zoobeen1@gmail.com>
2022-07-05 22:12:36 +0300

    Update form.jsx

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -37,6 +37,7 @@ export const InputForm = ({ onSubmit }) => {
         <Input
           type="text"
           name="name"
+          id="name"
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           placeholder="Add a new contact"
@@ -49,6 +50,7 @@ export const InputForm = ({ onSubmit }) => {
         <Input
           type="tel"
           name="number"
+          id="number"
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           placeholder="Add a phone number"
